a2376e1a1b34c94f526c6048a8fa984149c8f2f1
scatterbrain3 <scattini3@hotmail.com>
2019-11-08 16:38:06 +1030

    initial commit

---
+++ src/Box.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+
+class Box extends Component{
+    render(){
+        return(
+            <div>
+                <div style={{
+                    height: `${this.props.height}px`,
+                    width: `${this.props.width}px`,
+                    backgroundColor: this.props.color
+                }}>
+                </div>
+                <button>X</button>
+            </div>
+        );
+    }
+}
+
+export default Box;
